11cf45238d1e4bec5638486267bfc8b3bfd12ed4
Matteo Corradini <corra.matteo@gmail.com>
2017-05-25 19:05:46 +0200

    Add signup app

--- seller/models.py
+++ seller/models.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from signup.models import Seller
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
 
+# Create your models here.
 class Feedback(models.Model):
     #author = models.ForeignKey(Customer, on_delete=models.CASCADE)
     #seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
--- signup/models.py
+++ signup/models.py
@@ -1,23 +1,41 @@
 from django.db import models
 from django.core.validators import RegexValidator
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 import os, uuid
 
-# Create your models here.
 
+# Create your models here.
 def seller_image_path(instance, filename):
     ext = filename.split('.')[-1]
     return os.path.join('img/seller', '{}.{}'.format(uuid.uuid4(), ext))
 
+
+class User(AbstractUser):
+    address = models.CharField(max_length=50)
+    sub_date = models.DateTimeField('date subscription')
+    tel_number = models.CharField(max_length=20)
+
+
 class Seller(models.Model):
-    name = models.CharField(max_length=50)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     iva = models.CharField(validators=[
-        RegexValidator(regex='^.{11}$', message='Length has to be 11',
+        RegexValidator(regex='^.{16}$', message='Length has to be 16',
                        code='nomatch')
     ], max_length=11)
-    address = models.CharField(max_length=50)
-    image = models.ImageField(upload_to=seller_image_path, max_length=100)
-    sub_date = models.DateTimeField('date subscription')
+    logo = models.ImageField(upload_to=seller_image_path, max_length=100)
 
     def __str__(self):
         return self.name
+
+
+# Custom User model
+# https://simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html#onetoone
+class Customer(models.Model):
+    #birth_date = models.DateField(null=True, blank=True)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    cf = models.CharField(validators=[
+        RegexValidator(regex='^.{11}$', message='Length has to be 11',
+                       code='nomatch')
+    ], max_length=11)
